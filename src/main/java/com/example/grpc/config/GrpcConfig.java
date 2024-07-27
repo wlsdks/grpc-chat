cb9eeb3b19bd9bce0eefde4c7496e11be55b9bc7
wlsdks <wlsdks12@naver.com>
2024-07-27 19:18:27 +0900

    :recycle: 필요없는 롬복코드 제거

--- src/main/java/com/example/grpc/config/GrpcConfig.java
+++ src/main/java/com/example/grpc/config/GrpcConfig.java
@@ -1,6 +1,5 @@
 package com.example.grpc.config;
 
-import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 /**
  * gRPC 서버 설정 - 인터셉터를 등록하는 클래스
  */
-@RequiredArgsConstructor
 @Configuration
 public class GrpcConfig {
 
