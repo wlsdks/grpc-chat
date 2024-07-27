22bee2af541fc5edb3f184ba298e46df8a418341
wlsdks <wlsdks12@naver.com>
2024-07-27 19:20:31 +0900

    :recycle: 빈 어노테이션 제거 (Grpc 어노테이션에 들어있다.)

--- src/main/java/com/example/grpc/config/GrpcConfig.java
+++ src/main/java/com/example/grpc/config/GrpcConfig.java
@@ -1,7 +1,6 @@
 package com.example.grpc.config;
 
 import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,7 +12,6 @@ public class GrpcConfig {
     /**
      * gRPC 예외 처리 인터셉터 설정
      */
-    @Bean
     @GrpcGlobalServerInterceptor
     ExceptionHandlingInterceptor exceptionHandlingInterceptor() {
         return new ExceptionHandlingInterceptor();
