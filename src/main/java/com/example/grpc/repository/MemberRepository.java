7560e317512db0befc1863b86d09a080415d10aa
wlsdks <wlsdks12@naver.com>
2024-07-27 17:09:32 +0900

    :sparkles: 엔티티, DTO, 리포지토리 추가

---
+++ src/main/java/com/example/grpc/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.example.grpc.repository;
+
+import com.example.grpc.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    
+}
